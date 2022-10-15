4360c2df46a8458476b0a3acc1844d4ae926c262
Jagger Abney <jagger.abney@gmail.com>
2022-10-14 23:03:39 -0700

    extracted part of ExpenseItem to ExpenseDate

--- project-1/src/components/ExpenseItem.js
+++ project-1/src/components/ExpenseItem.js
@@ -1,10 +1,7 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   const amount = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -12,11 +9,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{amount}</div>
