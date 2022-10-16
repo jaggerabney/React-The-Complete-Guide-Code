582a56805ac6c54478988dc8d3e3dfd39b52a4a9
Jagger Abney <jagger.abney@gmail.com>
2022-10-15 23:14:33 -0700

    consolidated ExpenseForm state to one object

--- project-1/src/components/NewExpense/ExpenseForm.js
+++ project-1/src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,35 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredAmount, setEnteredAmount] = useState("");
+  //   const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   function titleChangeHandler(event) {
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   }
 
   function amountChangeHandler(event) {
-    setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   }
 
   function dateChangeHandler(event) {
-    setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   }
 
   return (
