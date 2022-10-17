7a1b7dacb31083f9dab9ae0a51b1c4666ca6ea2d
Jagger Abney <jagger.abney@gmail.com>
2022-10-16 18:18:55 -0700

    wrapped ExpenseItem in li tag

--- project-1/src/components/Expenses/ExpenseItem.js
+++ project-1/src/components/Expenses/ExpenseItem.js
@@ -11,13 +11,15 @@ function ExpenseItem(props) {
   }).format(props.amount);
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
