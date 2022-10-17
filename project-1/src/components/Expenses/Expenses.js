e7f6d2c2fdcb486d42f47a30fdd4b2e0480afac5
Jagger Abney <jagger.abney@gmail.com>
2022-10-16 18:10:57 -0700

    added component when there are no expenses

--- project-1/src/components/Expenses/Expenses.js
+++ project-1/src/components/Expenses/Expenses.js
@@ -7,6 +7,21 @@ import "./Expenses.css";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState("2022");
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear() === Number(selectedYear)
+  );
+  let expensesContent = <p>No expenses found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   function changeHandler(event) {
     setSelectedYear(event.target.value);
@@ -16,18 +31,7 @@ function Expenses(props) {
     <Fragment>
       <Card className="expenses">
         <ExpensesFilter selected={selectedYear} onChange={changeHandler} />
-        {props.expenses
-          .filter(
-            (expense) => expense.date.getFullYear() === Number(selectedYear)
-          )
-          .map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        {expensesContent}
       </Card>
     </Fragment>
   );
