bde04fa083c077cf956b0f2bbc4921eae5f22c1a
Jagger Abney <jagger.abney@gmail.com>
2022-10-16 18:18:01 -0700

    extracted content from Expenses to ExpenseList

--- project-1/src/components/Expenses/Expenses.js
+++ project-1/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 
 import ExpensesFilter from "./ExpensesFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -10,18 +10,6 @@ function Expenses(props) {
   const filteredExpenses = props.expenses.filter(
     (expense) => expense.date.getFullYear() === Number(selectedYear)
   );
-  let expensesContent = <p>No expenses found!</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   function changeHandler(event) {
     setSelectedYear(event.target.value);
@@ -31,7 +19,7 @@ function Expenses(props) {
     <Fragment>
       <Card className="expenses">
         <ExpensesFilter selected={selectedYear} onChange={changeHandler} />
-        {expensesContent}
+        <ExpenseList expenses={filteredExpenses} />
       </Card>
     </Fragment>
   );
