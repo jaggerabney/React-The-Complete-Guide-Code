61cb6b7a5b326ce0beb5e883920131aa9bcbcc9c
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-10-31 12:51:24 -0700

    added base code for project-6

---
+++ project-6/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import Button from "./components/UI/Button/Button";
+
+import "./App.css";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  function toggleParagraphHandler() {
+    setShowParagraph((prevState) => !prevState);
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
