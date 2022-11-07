740f0c7ec78cf9404ac657195c7ccd64ecb0a910
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-11-07 13:15:43 -0800

    extracted counter logic to useCounter hook

---
+++ project-9/src/hooks/use-counter.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function useCounter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+}
+
+export default useCounter;
