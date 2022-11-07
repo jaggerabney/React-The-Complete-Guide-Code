d0df059cf7dd08758bb846355854343619017530
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-11-07 13:52:04 -0800

    changed App to utilize useHttp hook

--- project-10/src/App.js
+++ project-10/src/App.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  function transformTasks(tasks) {
+    const loadedTasks = [];
+
+    for (const taskKey in tasks) {
+      loadedTasks.push({ id: taskKey, text: tasks[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    { url: "https://react-http-facab-default-rtdb.firebaseio.com/tasks.json" },
+    transformTasks
+  );
+
   useEffect(() => {
     fetchTasks();
   }, []);
