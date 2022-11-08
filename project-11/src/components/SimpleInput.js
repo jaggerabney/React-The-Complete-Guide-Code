a1746d360bc237ee1d7483f4d33d711c6b55f87a
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-11-08 15:25:28 -0800

    added email input field to SimpleInput

--- project-11/src/components/SimpleInput.js
+++ project-11/src/components/SimpleInput.js
@@ -1,49 +1,80 @@
 import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
-  const [inputValue, setInputValue] = useState("");
-  const [inputTouched, setInputTouched] = useState(false);
+  const [nameInputValue, setNameInputValue] = useState("");
+  const [nameInputTouched, setNameInputTouched] = useState(false);
+  const [emailInputValue, setEmailInputValue] = useState("");
+  const [emailInputTouched, setEmailInputTouched] = useState(false);
 
-  const inputValueIsValid = inputValue.trim() !== "";
-  const inputIsInvalid = !inputValueIsValid && inputTouched;
+  const nameInputValueIsValid = nameInputValue.trim() !== "";
+  const nameInputIsInvalid = !nameInputValueIsValid && nameInputTouched;
+  const emailInputValueIsValid =
+    emailInputValue.trim() !== "" && emailInputValue.includes("@");
+  const emailInputIsInvalid = !emailInputValueIsValid && emailInputTouched;
   let formIsValid = false;
 
-  if (inputValueIsValid) {
+  if (nameInputValueIsValid && emailInputValueIsValid) {
     formIsValid = true;
   }
 
   function submitHandler(event) {
     event.preventDefault();
 
-    if (inputValueIsValid) {
-      setInputTouched(false);
-      setInputValue("");
+    if (nameInputValueIsValid && emailInputValueIsValid) {
+      setNameInputTouched(false);
+      setNameInputValue("");
+
+      setEmailInputTouched(false);
+      setEmailInputValue("");
     }
   }
 
   function inputChangeHandler(event) {
-    setInputValue(event.target.value);
+    if (event.target.id === "name") {
+      setNameInputValue(event.target.value);
+    } else if (event.target.id === "email") {
+      setEmailInputValue(event.target.value);
+    }
   }
 
   function inputBlurHandler(event) {
-    setInputTouched(true);
+    if (event.target.id === "name") {
+      setNameInputTouched(true);
+    } else if (event.target.id === "email") {
+      setEmailInputTouched(true);
+    }
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <div className={`form-control ${inputIsInvalid && "invalid"}`}>
+      <div className={`form-control ${nameInputIsInvalid && "invalid"}`}>
         <label htmlFor="name">Your Name</label>
         <input
           onChange={inputChangeHandler}
           onBlur={inputBlurHandler}
-          value={inputValue}
+          value={nameInputValue}
           type="text"
           id="name"
         />
-        {inputIsInvalid && (
-          <p className="error-text">Name must not be empty.</p>
-        )}
       </div>
+      {nameInputIsInvalid && (
+        <p className="error-text">Name must not be empty.</p>
+      )}
+      <div className={`form-control ${emailInputIsInvalid && "invalid"}`}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          onChange={inputChangeHandler}
+          onBlur={inputBlurHandler}
+          value={emailInputValue}
+          type="text"
+          id="email"
+        />
+      </div>
+      {emailInputIsInvalid && (
+        <p className="error-text">
+          Email must not be empty and must contain an @.
+        </p>
+      )}
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
       </div>
