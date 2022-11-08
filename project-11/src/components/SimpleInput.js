c3ad06d6c82711ec1d65b995df84b4c773c1b979
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-11-08 14:35:36 -0800

    added MORE validation to SimpleInput

--- project-11/src/components/SimpleInput.js
+++ project-11/src/components/SimpleInput.js
@@ -1,11 +1,14 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [inputValue, setInputValue] = useState("");
-  const [inputValueIsValid, setInputValueIsValid] = useState(true);
+  const [inputValueIsValid, setInputValueIsValid] = useState(false);
+  const [inputTouched, setInputTouched] = useState(false);
 
   function submitHandler(event) {
     event.preventDefault();
+
+    setInputTouched(true);
 
     if (inputValue.trim() === "") {
       setInputValueIsValid(false);
@@ -19,19 +22,35 @@ const SimpleInput = (props) => {
 
   function inputChangeHandler(event) {
     setInputValue(event.target.value);
+
+    if (event.target.value.trim() !== "") {
+      setInputValueIsValid(true);
+    }
   }
+
+  function inputBlurHandler(event) {
+    setInputTouched(true);
+
+    if (inputValue.trim() === "") {
+      setInputValueIsValid(false);
+      return;
+    }
+  }
+
+  const inputIsInvalid = !inputValueIsValid && inputTouched;
 
   return (
     <form onSubmit={submitHandler}>
-      <div className={`form-control ${inputValueIsValid ? "" : "invalid"}`}>
+      <div className={`form-control ${inputIsInvalid && "invalid"}`}>
         <label htmlFor="name">Your Name</label>
         <input
           onChange={inputChangeHandler}
+          onBlur={inputBlurHandler}
           value={inputValue}
           type="text"
           id="name"
         />
-        {!inputValueIsValid && (
+        {inputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
         )}
       </div>
