fb978e5935dff394e7e2785b49a5a372e4ce856e
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-11-08 14:08:21 -0800

    added ref to read value of SimpleInput on submit

--- project-11/src/components/SimpleInput.js
+++ project-11/src/components/SimpleInput.js
@@ -1,9 +1,19 @@
+import { useRef } from "react";
+
 const SimpleInput = (props) => {
+  const inputValueRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    console.log(inputValueRef.current.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input ref={inputValueRef} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
