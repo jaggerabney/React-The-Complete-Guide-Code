777fbf45ccb326c1cde41a3b50645c7b04538320
jaggerabney <jagger.abney@gmail.com>
2022-11-13 10:52:45 -0800

    added useHttp hook

--- project-5/src/App.js
+++ project-5/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useHttp from "./hooks/use-http";
+import DATABASE_URL from "./resources/database-url";
 
 import AvailableMeals from "./components/Meals/AvailableMeals";
 import MealsSummary from "./components/Meals/MealsSummary";
@@ -6,6 +8,14 @@ import Header from "./components/Header/Header";
 import { CartContextProvider } from "./store/cart-context";
 
 function App() {
+  const { isLoading, error, sendRequest } = useHttp();
+
+  useEffect(() => {
+    sendRequest({
+      url: DATABASE_URL,
+    });
+  }, []);
+
   return (
     <CartContextProvider>
       <Header />
