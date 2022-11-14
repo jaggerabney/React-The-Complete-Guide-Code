cf66b707775ba3f716ef879bfa53289870b6e93c
jaggerabney <jagger.abney@gmail.com>
2022-11-13 17:42:24 -0800

    refactored Checkout

--- project-5/src/components/Cart/Checkout.js
+++ project-5/src/components/Cart/Checkout.js
@@ -1,39 +1,96 @@
+import React from "react";
+
+import useInput from "../../hooks/use-input";
 import classes from "./Checkout.module.css";
 import CheckoutInput from "./CheckoutInput";
 
 function Checkout(props) {
   function submitHandler(event) {
     event.preventDefault();
+
+    nameReset();
+    streetReset();
+    cityReset();
+    ZIPReset();
   }
+
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: nameReset,
+  } = useInput((value) => value.length > 0);
+  const {
+    value: streetValue,
+    isValid: streetIsValid,
+    hasError: streetHasError,
+    inputChangeHandler: streetChangeHandler,
+    inputBlurHandler: streetBlurHandler,
+    reset: streetReset,
+  } = useInput((value) => value.length > 0);
+  const {
+    value: cityValue,
+    isValid: cityIsValid,
+    hasError: cityHasError,
+    inputChangeHandler: cityChangeHandler,
+    inputBlurHandler: cityBlurHandler,
+    reset: cityReset,
+  } = useInput((value) => value.length > 0);
+  const {
+    value: ZIPValue,
+    isValid: ZIPIsValid,
+    hasError: ZIPHasError,
+    inputChangeHandler: ZIPChangeHandler,
+    inputBlurHandler: ZIPBlurHandler,
+    reset: ZIPReset,
+  } = useInput((value) => value.length === 5 && /^\d+$/.test(value));
+
+  let formIsValid = nameIsValid && streetIsValid && cityIsValid && ZIPIsValid;
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <CheckoutInput
-        validationFunction={(value) => value.length > 0}
-        errorMessage="Your name must not be empty."
+        className={`${classes.control} ${nameHasError && classes.invalid}`}
+        onChange={nameChangeHandler}
+        onBlur={nameBlurHandler}
+        value={nameValue}
+        error={nameHasError}
+        errorMessage="Name must not be empty."
         label="Your Name"
         type="text"
         id="name"
       />
       <CheckoutInput
-        validationFunction={(value) => value.length > 0}
+        className={`${classes.control} ${streetHasError && classes.invalid}`}
+        onChange={streetChangeHandler}
+        onBlur={streetBlurHandler}
+        value={streetValue}
+        error={streetHasError}
         errorMessage="Street address must not be empty."
         label="Street Address"
         type="text"
         id="street"
       />
       <CheckoutInput
-        validationFunction={(value) => value.length > 0}
+        className={`${classes.control} ${cityHasError && classes.invalid}`}
+        onChange={cityChangeHandler}
+        onBlur={cityBlurHandler}
+        value={cityValue}
+        error={cityHasError}
         errorMessage="City must not be empty."
         label="City"
         type="text"
         id="city"
       />
       <CheckoutInput
-        validationFunction={(value) =>
-          value.length === 5 && /^\d+$/.test(value)
-        }
-        errorMessage="ZIP code must be a five-character number."
+        className={`${classes.control} ${ZIPHasError && classes.invalid}`}
+        onChange={ZIPChangeHandler}
+        onBlur={ZIPBlurHandler}
+        value={ZIPValue}
+        error={ZIPHasError}
+        errorMessage="ZIP code must be a five-digit number."
         label="ZIP Code"
         type="text"
         id="ZIP"
@@ -42,7 +99,9 @@ function Checkout(props) {
         <button type="button" onClick={props.onCancel}>
           Cancel
         </button>
-        <button className={classes.submit}>Confirm</button>
+        <button disabled={!formIsValid} className={classes.submit}>
+          Confirm
+        </button>
       </div>
     </form>
   );
--- project-5/src/components/Cart/CheckoutInput.js
+++ project-5/src/components/Cart/CheckoutInput.js
@@ -1,23 +1,19 @@
 import React from "react";
-import useInput from "../../hooks/use-input";
 
 import classes from "./CheckoutInput.module.css";
 
 function CheckoutInput(props) {
-  const inputState = useInput(props.validationFunction);
-
   return (
-    <div
-      className={`${classes.control} ${inputState.hasError && classes.invalid}`}
-    >
+    <div className={props.className}>
       <label htmlFor={props.id}>{props.label}</label>
       <input
-        onChange={inputState.inputChangeHandler}
-        onBlur={inputState.inputBlurHandler}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        value={props.value}
         type={props.type}
         id={props.id}
       />
-      {inputState.hasError && (
+      {props.error && (
         <p className={classes["error-text"]}>{props.errorMessage}</p>
       )}
     </div>
