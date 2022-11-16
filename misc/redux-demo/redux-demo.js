8194bec7765c81acc42b602ffe63974c5f973ca9
jaggerabney <jagger.abney@gmail.com>
2022-11-15 17:27:24 -0800

    added counterSubscriber

--- misc/redux-demo/redux-demo.js
+++ misc/redux-demo/redux-demo.js
@@ -1,9 +1,18 @@
 const redux = require("redux");
 
-function counterReducer(state, action) {
+function counterReducer(state = { counter: 0 }, action) {
   return {
     counter: state.counter + 1,
   };
 }
 
 const store = redux.createStore(counterReducer);
+
+function counterSubscriber() {
+  const latestState = store.getState();
+  console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "INCREMENT" });
