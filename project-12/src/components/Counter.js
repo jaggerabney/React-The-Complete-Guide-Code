95d338a95715239e6d49d0ac499c121c0015fb91
jaggerabney <jagger.abney@gmail.com>
2022-11-30 17:20:40 -0800

    added functionality to toggle counter button

--- project-12/src/components/Counter.js
+++ project-12/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   function incrementHandler() {
@@ -27,12 +28,14 @@ const Counter = () => {
     });
   }
 
-  const toggleCounterHandler = () => {};
+  function toggleCounterHandler() {
+    dispatch({ type: "TOGGLE" });
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement by 1</button>
         <button onClick={incrementHandler}>Increment by 1</button>
--- project-12/src/store/index.js
+++ project-12/src/store/index.js
@@ -1,13 +1,20 @@
 import { createStore } from "redux";
 
-function counterReducer(state = { counter: 0 }, action) {
+function counterReducer(state = { counter: 0, showCounter: true }, action) {
   if (action.type === "INCREASE") {
     return {
       counter: state.counter + action.value,
+      showCounter: state.showCounter,
     };
   } else if (action.type === "DECREMENT") {
     return {
       counter: state.counter - action.value,
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "TOGGLE") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 
