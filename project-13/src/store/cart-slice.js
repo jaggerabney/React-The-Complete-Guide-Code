9b5d4f44fe8c6376fe035429b3a36042ec575cc4
jaggerabney <jagger.abney@gmail.com>
2022-12-01 17:16:20 -0800

    Add to Cart button now works

--- project-13/src/store/cart-slice.js
+++ project-13/src/store/cart-slice.js
@@ -4,11 +4,23 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
+    totalQuantity: 0,
+    totalPrice: 0.0,
     isVisible: false,
   },
   reducers: {
     add(state, action) {
-      // add code here
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push(newItem);
+      } else {
+        existingItem.quantity += newItem.quantity;
+      }
+
+      state.totalQuantity += newItem.quantity;
+      state.price += newItem.price * newItem.quantity;
     },
     remove(state, action) {
       // add code here
