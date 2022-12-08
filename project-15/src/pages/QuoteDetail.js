74642fddaab6791d3a2e43824326828b730b93ad
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-12-08 15:00:43 -0800

    the View Fullscreen button in QuoteItem now works

--- project-15/src/pages/QuoteDetail.js
+++ project-15/src/pages/QuoteDetail.js
@@ -1,13 +1,26 @@
 import { Route, useParams } from "react-router-dom";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Jagger", text: "Learning React is great!" },
+  { id: "q3", author: "Kira", text: 'What the hell is a "React"?' },
+];
 
 function QuoteDetail(props) {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
