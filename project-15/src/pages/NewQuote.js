f680a40c1802d8d7aa8143dfb596d501060961dd
jaggerabney <jagger.abney@gmail.com>
2022-12-08 18:17:56 -0800

    quotes are now posted/fetched from db

--- project-15/src/pages/NewQuote.js
+++ project-15/src/pages/NewQuote.js
@@ -1,15 +1,27 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { addQuote } from "../lib/api";
+import useHttp from "../hooks/use-http";
 
 import QuoteForm from "../components/quotes/QuoteForm";
 
 function NewQuote(props) {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   function addQuoteHandler(quoteData) {
-    history.push("/quotes");
+    sendRequest(quoteData);
   }
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 }
 
 export default NewQuote;
--- project-15/src/pages/QuoteDetail.js
+++ project-15/src/pages/QuoteDetail.js
@@ -1,7 +1,11 @@
 import { Route, Link, useParams, useRouteMatch } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Max", text: "Learning React is fun!" },
@@ -10,18 +14,38 @@ const DUMMY_QUOTES = [
 ];
 
 function QuoteDetail(props) {
-  const params = useParams();
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+  const { quoteId } = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-  if (!quote) {
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
