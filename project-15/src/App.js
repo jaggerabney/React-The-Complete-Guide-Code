28951cc37e7b68acf552d23588dda6b847f117ac
jaggerabney <jagger.abney@gmail.com>
2022-12-08 16:18:41 -0800

    added a 404 page

--- project-15/src/App.js
+++ project-15/src/App.js
@@ -1,9 +1,11 @@
 import { Redirect, Route, Switch } from "react-router-dom";
+
 import Layout from "./components/layout/Layout";
 
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
