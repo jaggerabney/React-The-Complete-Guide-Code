61dc25ff892f2c4986c24e9a489f4b2bc01268a7
EXT-Abney, Jagger <jagger.abney@boeing.com>
2022-12-09 14:44:02 -0800

    added DeferredBlogPostsPage to mimic slow backend

--- misc/react-router-6.4-intro/src/App.jsx
+++ misc/react-router-6.4-intro/src/App.jsx
@@ -12,13 +12,20 @@ import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
 import RootLayout from "./components/RootLayout";
 import WelcomePage from "./pages/Welcome";
 import ErrorPage from "./pages/ErrorPage";
+import DeferredBlogPostsPage, {
+  loader as deferredBlogPostsLoader,
+} from "./pages/DeferredBlogPosts";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
       <Route index element={<WelcomePage />} />
       <Route path="/blog" element={<BlogLayout />}>
-        <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
+        <Route
+          index
+          element={<DeferredBlogPostsPage />}
+          loader={deferredBlogPostsLoader}
+        />
         <Route
           path=":id"
           element={<PostDetailPage />}
