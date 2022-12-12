1bc530ce5e580918413d444b97b9fadfdc05ca09
jaggerabney <jagger.abney@gmail.com>
2022-12-11 17:00:32 -0800

    added login functionality

--- project-16/src/components/Auth/AuthForm.js
+++ project-16/src/components/Auth/AuthForm.js
@@ -24,37 +24,46 @@ const AuthForm = () => {
     // optional: add validation
 
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+        API_KEY;
     } else {
-      fetch(
+      url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
-          API_KEY,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((response) => {
+        API_KEY;
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
         setIsLoading(false);
 
         if (response.ok) {
-          // ...
+          return response.json();
         } else {
           return response.json().then((data) => {
             let errorMessage = "Authentication failed!";
 
-            alert(errorMessage);
+            throw new Error(errorMessage);
           });
         }
-      });
-    }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => alert(error.message));
   }
 
   return (
