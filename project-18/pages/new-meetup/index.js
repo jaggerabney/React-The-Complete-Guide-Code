05f86b41b77c4e5009a6c8e6d162b008feeddc09
jaggerabney <jagger.abney@gmail.com>
2022-12-15 16:53:48 -0800

    added basic content to new-meetup

---
+++ project-18/pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(event) {
+    console.log(event);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
