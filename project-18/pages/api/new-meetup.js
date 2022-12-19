a6cbd0478386bb763174b70c4f30e3b1de1d8ba3
jaggerabney <jagger.abney@gmail.com>
2022-12-18 18:31:10 -0800

    added api route for new-meetup

--- project-18/pages/api/new-meetup.js
+++ project-18/pages/api/new-meetup.js
@@ -1,3 +1,21 @@
-function handler(req, res) {}
+import { MongoClient } from "mongodb";
+
+import CONNECTION_STRING from "../../resources/connection-string";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(CONNECTION_STRING);
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
 
 export default handler;
