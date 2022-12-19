58c04d9948be5ccbbcedf153f0acb2980c32f92e
jaggerabney <jagger.abney@gmail.com>
2022-12-18 18:42:23 -0800

    data entered in new-meetup page is now added to db

--- project-18/pages/new-meetup/index.js
+++ project-18/pages/new-meetup/index.js
@@ -1,9 +1,22 @@
-import Layout from "../../components/layout/Layout";
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(event) {
-    console.log(event);
+  const router = useRouter();
+
+  async function addMeetupHandler(data) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
