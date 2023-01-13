42a06dc327ba9ce3be093844c2eb50134f2fdd60
jaggerabney <jagger.abney@gmail.com>
2023-01-13 14:48:45 -0800

    "Show Details" button now works

--- project-18/pages/[meetupId]/index.js
+++ project-18/pages/[meetupId]/index.js
@@ -1,47 +1,59 @@
+import { MongoClient, ObjectId } from "mongodb";
 import { useRouter } from "next/router";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import CONNECTION_STRING from "../../resources/connection-string";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/e/eb/Mount_Rainier_from_west.jpg"
-      title="First meetup!"
-      address="39000 State Route 706 E, Ashford, WA 98304"
-      description="We met at the base of Mount Rainier!"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(CONNECTION_STRING);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(CONNECTION_STRING);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/e/eb/Mount_Rainier_from_west.jpg",
-        title: "First meetup!",
-        address: "39000 State Route 706 E, Ashford, WA 98304",
-        description: "We met at the base of Mount Rainier!",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
