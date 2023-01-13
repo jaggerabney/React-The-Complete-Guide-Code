41fb66f6ef0f697333d28ccc4be3019b052a5f4e
jaggerabney <jagger.abney@gmail.com>
2023-01-13 15:15:41 -0800

    added Heads to all pages

--- project-18/pages/index.js
+++ project-18/pages/index.js
@@ -1,29 +1,23 @@
 import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
+import Head from "next/head";
 
 import CONNECTION_STRING from "../resources/connection-string";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "First meetup!",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/e/eb/Mount_Rainier_from_west.jpg",
-    address: "39000 State Route 706 E, Ashford, WA 98304",
-    description: "We met at the base of Mount Rainier!",
-  },
-  {
-    id: "m2",
-    title: "Second meetup!",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/dc/MSH82_st_helens_plume_from_harrys_ridge_05-19-82.jpg",
-    address: "3029 Spirit Lake Hwy, Castle Rock, WA 98611",
-    description: "Then we met at Mount Saint Helens!",
-  },
-];
-
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 }
 
 export async function getStaticProps() {
