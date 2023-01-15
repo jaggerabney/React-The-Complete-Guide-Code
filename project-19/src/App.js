1172cc9a246d859a2fb9cc423dfd9f65029b76b2
jaggerabney <jagger.abney@gmail.com>
2023-01-14 16:36:13 -0800

    installed/tested React Transition Groups

--- project-19/src/App.js
+++ project-19/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 import "./App.css";
+
+import Transition from "react-transition-group/Transition";
+
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
@@ -8,6 +11,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal() {
@@ -22,11 +26,47 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal
-          show={this.state.modalIsOpen}
-          closed={this.hideModal.bind(this)}
-        />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                showBlock: !prevState.showBlock,
+              };
+            });
+          }}
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 300ms ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            />
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? (
+          <Modal
+            show={this.state.modalIsOpen}
+            closed={this.hideModal.bind(this)}
+          />
+        ) : null}
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.showModal.bind(this)}>
           Open Modal
         </button>
