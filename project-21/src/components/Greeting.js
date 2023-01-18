681895647da0cc98e1f4abf51375afbbfd4b7e51
jaggerabney <jagger.abney@gmail.com>
2023-01-17 16:41:44 -0800

    added (button/more tests) to (Greeting/test suite)

--- project-21/src/components/Greeting.js
+++ project-21/src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 export default function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  function changeTextHandler(event) {
+    setChangedText(true);
+  }
+
   return (
     <div>
       <h2>Hello world!</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed text!</p>}
+      <button onClick={changeTextHandler}>Change text!</button>
     </div>
   );
 }
--- project-21/src/components/Greeting.test.js
+++ project-21/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -14,5 +15,30 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText(/hello world/i);
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('Renders "It\'s good to see you!" pre-click', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // Nothing
+
+    // Assert
+    const igtsyElement = screen.getByText(/it's good to see you/i);
+    expect(igtsyElement).toBeInTheDocument();
+  });
+
+  test('Renders "Changed!" post-click', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const changedElement = screen.getByText(/changed/i);
+    expect(changedElement).toBeInTheDocument();
   });
 });
