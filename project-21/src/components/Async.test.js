838aeff5e323612e99a81f23a532615f95cef4f0
jaggerabney <jagger.abney@gmail.com>
2023-01-17 17:16:05 -0800

    changed Async test to use mock

--- project-21/src/components/Async.test.js
+++ project-21/src/components/Async.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
+
 import Async from "./Async";
 
 describe("Async component", () => {
   test("Renders posts if request succeeds", async () => {
     // Arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async function () {
+        return [{ id: "p1", title: "First post" }];
+      },
+    });
     render(<Async />);
 
     // Act
