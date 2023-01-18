2b5bcac58a855c2705e5d2d1dfd568f5bded4854
jaggerabney <jagger.abney@gmail.com>
2023-01-18 14:12:01 -0800

    exxplored type inference and union types

--- project-22/basics.ts
+++ project-22/basics.ts
@@ -1,5 +1,9 @@
+// There are two main kinds of types in TypeScript:
+
 // Primitives: number, string, boolean
 // Object-based: arrays, objects
+
+// -----------------------------------------------------
 
 // Primitive types
 
@@ -13,6 +17,8 @@ username = "Jagger"; // okay
 
 let isInstructor: boolean;
 isInstructor = false; // okay
+
+// -----------------------------------------------------
 
 // Object-based types
 
@@ -45,3 +51,21 @@ people = [
     age: 20,
   },
 ]; // okay
+
+// -----------------------------------------------------
+
+// Type inference
+
+// TypeScript infers which types are used where, even if not explicitly stated
+// Kind of like the "auto" keyword in C++
+let course = "React - The Complete Guide";
+// course = 12341;
+
+// -----------------------------------------------------
+
+// Union types
+
+// Variables of a union type can be either specified type of variable
+// i.e., course2 can be either a string or a number
+let course2: string | number = "React - The Complete Guide Code";
+course2 = 12341;
--- project-22/with-typescript.ts
+++ project-22/with-typescript.ts
@@ -1,6 +1,8 @@
+// @ts-ignore
 function add(a: number, b: number) {
   return a + b;
 }
 
+// @ts-ignore
 const result = add(2, 5);
 console.log(result);
